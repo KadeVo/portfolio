25fd1e157ce3fab8b725ed8e01a0fe49ae38ddcd
Kadin Vo <kadinvo@gmail.com>
2024-01-22 16:47:58 +1300

    Added bachelor

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import kadinImage from '../assets/images/kadin.jpg'
 import microsoftCert from '../assets/images/AZ-900.pdf'
+import bachelorCert from '../assets/images/bachelor.pdf'
 
 const About = () => {
   return (
@@ -15,19 +16,23 @@ const About = () => {
       </div>
       <div>
         <p className="text-base">
-          I graduated with a Bachelor of Business and transitioned my career
-          after uncovering my passion for software development whilst I was
-          coding as a hobby. I ended up falling in love with problem solving and
-          the art of creation. I decided to enroll into Dev Academy where they
-          gave me hands-on experience in a team environment.This hands-on
-          experience allowed me to cultivate my skills, not just as an
-          individual coder, but as a collaborative team player. I took on
-          challenges that mirrored real-world scenarios, honing my ability to
-          solve complex problems while working seamlessly within a team. I am
-          enthusiastic about creating innovative solutions and contributing to
-          meaningful projects. I am not just a developer; I am a passionate
-          problem solver, a team collaborator, and a lifelong learner who wants
-          to make a change. I recently deployed a website named{' '}
+          I graduated with a{' '}
+          <a href={bachelorCert} className="underline text-blue-500">
+            Bachelor of Business
+          </a>{' '}
+          and transitioned my career after uncovering my passion for software
+          development whilst I was coding as a hobby. I ended up falling in love
+          with problem solving and the art of creation. I decided to enroll into
+          Dev Academy where they gave me hands-on experience in a team
+          environment.This hands-on experience allowed me to cultivate my
+          skills, not just as an individual coder, but as a collaborative team
+          player. I took on challenges that mirrored real-world scenarios,
+          honing my ability to solve complex problems while working seamlessly
+          within a team. I am enthusiastic about creating innovative solutions
+          and contributing to meaningful projects. I am not just a developer; I
+          am a passionate problem solver, a team collaborator, and a lifelong
+          learner who wants to make a change. I recently deployed a website
+          named{' '}
           <a
             href="https://tailoredtails.onrender.com/"
             target="_blank"
@@ -40,7 +45,7 @@ const About = () => {
           <a href={microsoftCert} className="underline text-blue-500">
             {' '}
             AZ-900 Microsoft Exam.
-          </a>
+          </a>{' '}
           Currently learning Python and diving more into React Native.{' '}
         </p>
       </div>
