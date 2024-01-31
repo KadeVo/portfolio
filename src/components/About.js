c529613117e1da5438f50518f5e4a515ddfa77d0
Kadin Vo <kadinvo@gmail.com>
2024-02-01 00:36:39 +1300

    Adjusted about me

--- src/components/About.js
+++ src/components/About.js
@@ -18,17 +18,17 @@ const About = () => {
           </a>{' '}
           and transitioned my career after uncovering my passion for software
           development whilst I was coding as a hobby during University. I ended
-          up falling in love with problem solving and the art of creation. I
-          decided to enroll into Dev Academy where they gave me hands-on
-          experience in a team environment. This hands-on experience allowed me
-          to cultivate my skills, not just as an individual coder, but as a
-          collaborative team player & problem solver. I took on challenges that
-          mirrored real-world scenarios, honing my ability to solve complex
-          problems while working seamlessly within a team. I am enthusiastic
-          about creating innovative solutions and contributing to meaningful
-          projects. I am a passionate problem solver, a team collaborator, and a
-          lifelong learner who wants to make a change. I recently deployed a
-          website named{' '}
+          up falling in love with problem solving and the art of creating
+          software from scratch. I decided to enroll into Dev Academy where I
+          gained valuable hands-on experience in a team environment. The
+          immersive experience allowed me to cultivate my skills, not just as an
+          individual coder, but as a collaborative team player. I took on
+          challenges that mirrored real-world scenarios, honing my ability to
+          solve complex problems while working seamlessly within a team. I am
+          enthusiastic about creating innovative solutions and contributing to
+          meaningful projects. I am a passionate problem solver, a team
+          collaborator, and a fast learner who wants to make a change. I
+          recently deployed a website named{' '}
           <a
             href="https://tailoredtails.onrender.com/"
             target="_blank"
